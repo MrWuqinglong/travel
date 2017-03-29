b54afd4bf86a341b4a7cb895e856811c46c0110c
WuQinglong <wuqinglong.shell@gmail.com>
2017-03-29 16:20:12 +0800

    更改主页

--- src/main/java/com/controller/SiteController.java
+++ src/main/java/com/controller/SiteController.java
@@ -1,15 +1,16 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/site")
 @Controller
 public class SiteController {
 
-    @RequestMapping("/index.action")
+    @GetMapping("/index.action")
     public String toIndex() {
-        return "/index";
+        return "/site_index";
     }
 
 }
