fe45ec39fca54d5b594044309bc73d0d45248efa
wuqinglong.shell@gmail.com <wuqinglong.shell@gmail.com>
2017-04-14 18:54:12 +0800

    更改数据

---
+++ src/main/java/com/controller/BaseController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import com.system.web.Response;
+
+/**
+ * Created by 吴庆龙
+ * 2017/4/14 18:52
+ */
+public class BaseController {
+
+    protected Response response = new Response();
+
+}
--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.controller;
 
 import com.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.annotation.Resource;
+
 @Controller
 @RequestMapping({"/user", "/site"})
 @SessionAttributes(names = {"user"})
-public class UserController {
+public class UserController extends BaseController{
 
-    @Autowired
+    @Resource
     private UserService userService;
 
     @GetMapping("/index.action")
